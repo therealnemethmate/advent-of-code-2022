0460e37a49a935b0e7478f1909cc7b5dd5b11ba6
Mate Nemeth <mate.nemeth@oander.hu>
2022-12-08 21:10:16 +0100

    feat: 01 day js solution part2

--- 01/js/main.js
+++ 01/js/main.js
@@ -1,10 +1,23 @@
 import { readInputFile } from "../../utils/js/index.js";
 
 const input = await readInputFile(process.env.INPUT_MODE, '01');
-const maxCalories = Math.max(
-    ...input.split('\n\n').reduce(
-        (prev, curr) => prev.concat(curr.split('\n').reduce((sum, n) => sum += Number(n), 0)), []
-    )
-);
 
-console.log('The max calories an elf is carrying:', { maxCalories });
+/**
+ * @param {string[] | number[]} arr 
+ * @returns the summarized value of the numbers
+ */
+function sum(arr) {
+    return arr.reduce((sum, n) => sum += Number(n), 0);
+}
+
+function getSumCalories(input) {
+    return input.split('\n\n').reduce(
+        (prev, curr) => prev.concat(sum(curr.split('\n'))), []
+    )
+}
+
+(function main() {
+    const maxCalories = Math.max(...getSumCalories(input));
+    const top3Calories = sum(getSumCalories(input).sort((a, b) => b - a).slice(0, 3));
+    console.log('Results:', { maxCalories, top3Calories });    
+})();
