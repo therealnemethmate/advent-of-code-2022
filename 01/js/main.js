11ca9eda7ab684268a1a3416364d5be837afbff0
Mate Nemeth <mate.nemeth@oander.hu>
2022-12-08 13:08:18 +0100

    refactor: day 01 js task solution

--- 01/js/main.js
+++ 01/js/main.js
@@ -1,9 +1,8 @@
 import { readInputFile } from "../../utils/js/index.js";
 
 const input = await readInputFile(process.env.INPUT_MODE, '01');
-const maxCalories = Math.max(...input.split('\n').reduce((prev, curr) => {
-    curr === '' ? prev.push(0) : (prev[prev.length - 1] += Number(curr));
-    return prev;
-}, [0]));
+const maxCalories = Math.max(...input.split('\n\n').reduce((prev, curr) => {
+    return prev.concat(curr.split('\n').reduce((prevNumber, n) => prevNumber += Number(n), 0));
+}, []));
 
 console.log('The max calories an elf is carrying:', { maxCalories });
