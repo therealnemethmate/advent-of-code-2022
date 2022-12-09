ddf0a8c4320645962deb19d3cec3d6eaf69bbf1f
Mate Nemeth <mate.nemeth@oander.hu>
2022-12-09 07:40:04 +0100

    feat: day 01 part2 js solution go

--- 01/go/main.go
+++ 01/go/main.go
@@ -15,22 +15,58 @@ func readFile() string {
 	return string(buffer)
 }
 
-func main() {
-	var content string = readFile()
-	var split []string = strings.Split(content, "\n\n")
+func sumStingNumbers(arr []string) int {
+	sum := 0
+	for _, amount := range arr {
+		var converted, _ = strconv.Atoi(amount)
+		sum += converted
+	}
+	return sum
 
+}
+
+func sum(arr []int) int {
+	sum := 0
+	for _, amount := range arr { sum += amount }
+	return sum
+}
+
+func getMaxCalories(content string) int {
 	var max int = 0
-	for _, v := range split {
-		sum := 0
+	var splitContent = strings.Split(content, "\n\n")
+	for _, v := range splitContent {
 		var line []string = strings.Split(v, "\n")
-		for _, amount := range line {
-			converted, _ := strconv.Atoi(amount)
-			sum += converted
-		}
-		if sum > max {
-			max = sum
+		var lineSum = sumStingNumbers(line)
+		if lineSum > max { max = lineSum }
+	}
+	return max
+}
+
+func getSumOfTop3(content string) int {
+	var splitContent = strings.Split(content, "\n\n")
+	var top3 = []int{ 0, 0, 0 }
+	for _, v := range splitContent {
+		var line []string = strings.Split(v, "\n")
+		var lineSum = sumStingNumbers(line)
+		if top3[0] < lineSum {
+			/* linked list would be nice */
+			top3[2] = top3[1]
+			top3[1] = top3[0]
+			top3[0] = lineSum
+		} else if top3[1] < lineSum {
+			top3[2] = top3[1]
+			top3[1] = lineSum
+		} else if top3[2] < lineSum {
+			top3[2] = lineSum
 		}
 	}
+	return sum(top3)
+}
 
-	fmt.Printf("Max is %v", max)
+func main() {
+	var content string = readFile()
+	var max int = getMaxCalories(content)
+	var sumOfTop3 int = getSumOfTop3(content)
+	fmt.Printf("Max is %v\n", max)
+	fmt.Printf("Sum of top3 is %v\n", sumOfTop3)
 }
