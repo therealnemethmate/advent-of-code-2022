d1cd7451e5233f40e89118c3c369292dc30d7ad6
Mate Nemeth <mate.nemeth@oander.hu>
2022-12-14 12:04:55 +0100

    feat: day 05 part2 js solution

--- 05/js/main.js
+++ 05/js/main.js
@@ -1,32 +1,35 @@
 import { readInputFile } from "../../utils/js/index.js";
 
-const input = await readInputFile(process.env.INPUT_MODE, '05');
-const [ rawBoard, moves ] = input.split('\n\n');
-const rows = rawBoard.split('\n')
-const board = rows
-    .map((row, rowIndex) => row.split('')
-        .map((c, i) => {
-            const isLetter = c.match(/[A-Z]/);
-            if (isLetter) return `${c}${rows[rows.length - 1][i]}${rowIndex}`;
-            return c;
-        }
-    ))
-    .flat()
-    .filter((item) => item.length === 3)
-    .reduce((acc, curr) => {
-        let [ item, colIndex, rowIndex ] = curr;
-        colIndex = Number(colIndex);
-        rowIndex = Number(rowIndex);
-        if (acc[colIndex]) {
+
+function getBoard(rawBoard) {
+    const rows = rawBoard.split('\n')
+    return rows
+        .map((row, rowIndex) => row.split('')
+            .map((c, i) => {
+                const isLetter = c.match(/[A-Z]/);
+                if (isLetter) return `${c}${rows[rows.length - 1][i]}${rowIndex}`;
+                return c;
+            }
+        ))
+        .flat()
+        .filter((item) => item.length === 3)
+        .reduce((acc, curr) => {
+            let [ item, colIndex, rowIndex ] = curr;
+            colIndex = Number(colIndex);
+            rowIndex = Number(rowIndex);
+            if (acc[colIndex]) {
+                acc[colIndex][rowIndex] = item;
+                return acc;
+            } 
+            acc[colIndex] = new Array(rows.length - 1).fill(undefined);
             acc[colIndex][rowIndex] = item;
             return acc;
-        } 
-        acc[colIndex] = new Array(rows.length - 1).fill(undefined);
-        acc[colIndex][rowIndex] = item;
-        return acc;
-    }, [])
-    .map((col) => col.filter((item) => item));
-    
+        }, [])
+        .map((col) => col.filter((item) => item));
+}
+
+const input = await readInputFile(process.env.INPUT_MODE, '05');
+const [ rawBoard, moves ] = input.split('\n\n');
 const moveList = moves.split('\n').reduce((acc, instruction) => {
     if(!instruction) return acc;
     const [ amount, from, to ] = instruction.match(/\b([0-9]|[1-9][0-9])\b/g);
@@ -38,10 +41,20 @@ const moveList = moves.split('\n').reduce((acc, instruction) => {
     return acc;
 }, []);
 
+const boardPart1 = getBoard(`${rawBoard}`);
 moveList.forEach((move) => {
     for (let i = 1; i <= move.amount; i++) {
-        board[move.to].unshift(board[move.from].shift());        
+        boardPart1[move.to].unshift(boardPart1[move.from].shift());        
     }
 });
+console.log(boardPart1.map((item) => item[0]).join(''));
 
-console.log(board.map((item) => item[0]).join(''));
+const boardPart2 = getBoard(`${rawBoard}`);
+moveList.forEach((move) => {
+    const items = [];
+    for (let i = 1; i <= move.amount; i++) {
+        items.push(boardPart2[move.from].shift());
+    }
+    boardPart2[move.to] = items.concat(boardPart2[move.to]);
+});
+console.log(boardPart2.map((item) => item[0]).join(''));
